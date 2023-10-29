55597b99ae819c7b14c5228ea04a88276d3f403a
Tudor Anghelina <tudor.anghelina91@gmail.com>
2023-10-29 21:13:09 +0200

    Add Window Message Examples

---
+++ src/Win32API.Module1.WindowMessages/Main.cpp
@@ -0,0 +1,52 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+	const wchar_t CLASS_NAME[] = L"Sample Window Class";
+
+	WNDCLASS wc = { };
+
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	HWND hwnd = CreateWindowEx(
+		0,
+		CLASS_NAME,
+		L"Learn to Program Windows",
+		WS_OVERLAPPEDWINDOW,
+
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (hwnd == NULL)
+	{
+		return 1;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+
+	MSG msg = { };
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+		if (msg.message == WM_DESTROY)
+		{
+			PostQuitMessage(0);
+			return 0;
+		}
+	}
+}
